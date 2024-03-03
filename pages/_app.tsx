51d5b41a32b25201734b3bad738592d6ab51c813
seba1342 <sbailouni@gmail.com>
2024-03-03 18:02:27 +1100

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
 import "@/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
