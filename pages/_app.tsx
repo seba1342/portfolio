9497a2ff990e8b530de02b1988533d4bdd2538cb
seba1342 <sbailouni@gmail.com>
2024-03-03 18:03:23 +1100

    Add speed insights
    I am speed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
 import "@/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </Layout>
   );
 }
