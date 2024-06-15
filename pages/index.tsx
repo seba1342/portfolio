12e8fe6209cc559643c7f361cae11ee17f415f65
seba1342 <sbailouni@gmail.com>
2024-06-15 15:56:58 +1000

    Update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,22 @@
-import type { InferGetStaticPropsType } from "next";
-
+import CensoredText from "@/components/CensoredText/CensoredText";
 import { Titles } from "@/components/text/Titles";
-import { getAllProjectIds } from "@/lib/projects";
 
-export async function getStaticProps() {
-  const allProjects = getAllProjectIds();
-  return {
-    props: {
-      allProjects,
-    },
-  };
-}
-
-export default function Home({
-  allProjects,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() {
   return (
-    <div className="flex-col h-full">
-      <div className="h-1/2 md:h-2/3" />
-      <div className="pb-12 md:pb-24">
-        <Titles.H1>
-          I am a developer and designer who loves the intersection between
-          technology and art.
-        </Titles.H1>
-        <Titles.H1>
-          This website is a showcase of just a handful of projects. It also is a
-          playground for me to explore new ideas. I love learning about creative
-          ways to push technology beyond its design.
-        </Titles.H1>
-        <Titles.H1>
-          While my toolkit is always evolving, it currently includes Typescript,
-          React, React Native, Ruby, Rails, GraphQL.
-        </Titles.H1>
-      </div>
+    <div className="flex flex-col max-w-7xl min-h-full justify-end md:pt-24">
+      {/* Can't decide whether I like this first line or not... */}
+      {/* <Titles.H1>
+        Why the <CensoredText maskedWord="f**k" word="fuck" /> is writing an
+        intro for your own website so hard?
+      </Titles.H1> */}
+      <Titles.H1>
+        I use code to build engaging interactions and experiences. I love the
+        crossover between design and computing.
+      </Titles.H1>
+      <Titles.H1>
+        While my toolbox is always evolving, it currently includes Typescript,
+        React, React Native, Ruby, Rails, GraphQL.
+      </Titles.H1>
     </div>
   );
 }
