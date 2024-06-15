f5129a453d601f6bc897b5c4a05d4ba8faf3f8e4
seba1342 <sbailouni@gmail.com>
2024-06-15 16:09:55 +1000

    Update mobile header styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,33 +4,33 @@ import { usePathname } from "next/navigation";
 import { Mono } from "../text/Mono";
 
 export default function Header() {
-  const pathname = usePathname();
   return (
     <header className="fixed w-full z-50">
       <div className="flex justify-between items-center p-6 h-[81px] bg-oatmeal">
-        <h1 className="font-sans text-2xl">
+        <h1 className="font-sans text-xl md:text-2xl">
           <Link href="/">Sebastien Bailouni</Link>
         </h1>
         <ul className="flex flex-row justify-center items-center">
-          <li className="px-4">
-            <Link
-              className={pathname.includes("inspiration") ? `underline` : ""}
-              href="/inspiration"
-            >
-              <Mono.Body className="mb-[0px]">Inspiration</Mono.Body>
-            </Link>
-          </li>
-          <li className="px-4">
-            <Link
-              className={pathname.includes("projects") ? `underline` : ""}
-              href="/projects"
-            >
-              <Mono.Body className="mb-[0px]">Projects</Mono.Body>
-            </Link>
-          </li>
+          <Item type="inspiration" />
+          <Item type="projects" />
         </ul>
       </div>
       <div className="h-8 w-full bg-gradient-to-t from-transparent to-oatmeal" />
     </header>
+  );
+}
+
+function Item({ type }: { type: "inspiration" | "projects" }) {
+  const pathname = usePathname();
+
+  return (
+    <li className="px-2 md:px-4">
+      <Link
+        className={pathname.includes(type) ? `underline` : ""}
+        href={`/${type}`}
+      >
+        <Mono.Body className="mb-[0px] text-xs capitalize">{type}</Mono.Body>
+      </Link>
+    </li>
   );
 }
