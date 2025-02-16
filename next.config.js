74e0e52abab1a613f3403e0caad83036ea3aa0bf
seba1342 <sbailouni@gmail.com>
2025-02-16 17:00:54 +1100

    Don't transpile geist

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
     remotePatterns: [{ hostname: "images.are.na", protocol: "https" }],
   },
   reactStrictMode: true,
-  transpilePackages: ["geist"],
 };
 
 module.exports = nextConfig;
